340292764671b6301af4a27b876b78313452538e
Salvi <s.perezranchal@gmail.com>
2025-04-17 13:08:07 +0200

    created component header, added assets directory, bootstrap, favicon and title

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'core-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  public userRegistered: boolean = false;
 }
