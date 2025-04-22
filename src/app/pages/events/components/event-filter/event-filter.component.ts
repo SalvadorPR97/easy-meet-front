134b164f1a8383413c9404b0aae15e8e9eb28d85
spranchal <spranchal@indra.es>
2025-04-22 13:15:28 +0200

    components created and placed in events main

---
+++ src/app/pages/events/components/event-filter/event-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pages-events-event-filter',
+  imports: [],
+  templateUrl: './event-filter.component.html',
+  styleUrl: './event-filter.component.css'
+})
+export class EventFilterComponent {
+
+}
--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {LocationMapComponent} from './components/location-map/location-map.component';
+import {EventImgComponent} from './components/event-img/event-img.component';
+import {EventFilterComponent} from './components/event-filter/event-filter.component';
+import {EventsListComponent} from './components/events-list/events-list.component';
 
 @Component({
   selector: 'pages-events',
-  imports: [],
+  imports: [
+    LocationMapComponent,
+    EventImgComponent,
+    EventFilterComponent,
+    EventsListComponent
+  ],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
