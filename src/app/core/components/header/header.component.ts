548c4152e5b86eb503a7e091c456b912858c745e
Salvi <s.perezranchal@gmail.com>
2025-06-10 19:33:37 +0200

    fix in header to add profile pic

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'core-header',
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  public profile_picture: string = "assets/css/usuario.png";
+  public imgUrl = environment.imgUrl;
+
   constructor(public authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('profile_pic')) {
+      this.profile_picture = this.imgUrl + localStorage.getItem('profile_pic');
+    } else {
+      this.authService.profilePicChanged$.subscribe(
+        response => {
+          this.profile_picture = this.imgUrl + response;
+        }
+      )
+    }
   }
 
   onLogout() {
