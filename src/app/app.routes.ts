7f1ecdf7e7dd555ba89e0e918af4329da6c011bd
Salvi <s.perezranchal@gmail.com>
2025-06-13 09:49:52 +0200

    who we are component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import {guestGuard} from './core/guards/guest.guard';
 import {CreateEventComponent} from './pages/create-event/create-event.component';
 import {authGuard} from './core/guards/auth.guard';
 import {MyEventsComponent} from './pages/my-events/my-events.component';
+import {WhoWeAreComponent} from './pages/who-we-are/who-we-are.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent},
@@ -15,4 +16,5 @@ export const routes: Routes = [
   { path: 'eventos', component: EventsComponent},
   { path: 'eventos/crear', component: CreateEventComponent, canActivate: [authGuard]},
   { path: 'mis-eventos', component: MyEventsComponent, canActivate: [authGuard]},
+  { path: 'quienes-somos', component: WhoWeAreComponent},
 ];
